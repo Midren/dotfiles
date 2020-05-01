95f45e554dfc8c51898037bc2e6787374d2d07bb
Roman Milishchuk <milromchuk@gmail.com>
2020-05-01 03:59:43 +0300

    Move files according to usage

--- vim_runtime/install_awesome_vimrc.sh
+++ vim_runtime/install_awesome_vimrc.sh
@@ -3,8 +3,6 @@ set -e
 
 cd ~/.vim_runtime
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
