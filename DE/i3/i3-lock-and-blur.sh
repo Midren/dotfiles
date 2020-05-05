89fd4afcd89e4fafac508f4d9543c06537fcc148
Roman Milishchuk <milromchuk@gmail.com>
2020-05-05 16:16:25 +0300

    Minor changes

--- DE/i3/i3-lock-and-blur.sh
+++ DE/i3/i3-lock-and-blur.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 # set the icon and a temporary location for the screenshot to be stored
-#icon="$HOME/Pictures/Wallpapers/kitties.jpeg"
+icon="$HOME/.config/i3/lock_screen.png"
 tmpbg='/tmp/screen.png'
 
 # take a screenshot
-scrot "$tmpbg"
+#scrot -uzoa "$tmpbg"
+flameshot full -r > "$tmpbg"
 
 # blur the screenshot by resizing and scaling back up
 convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
 
 # overlay the icon onto the screenshot
-#convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
 
 # lock the screen with the blurred screenshot
 i3lock -i "$tmpbg" -f
