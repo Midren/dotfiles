4df3d3b18f3c3fa13781efd9f0cd8d356ebc9cab
Roman Milishchuk <milromchuk@gmail.com>
2020-05-20 02:49:28 +0300

    Add dynamic layout for terminal and opening in same directory

---
+++ DE/i3/i3-cwd-shell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# i3 thread: https://faq.i3wm.org/question/150/how-to-launch-a-terminal-from-here/?answer=152#post-id-152
+# From: https://gist.github.com/viking/5851049
+
+args=("$@")
+
+TERM=${args[0]}
+CWD=''
+
+# Get window ID
+ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
+
+# Get PID of process whose window this is
+PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
+
+# Get last child process (shell, vim, etc)
+if [ -n "$PID" ]; then
+  TREE=$(pstree -lpA $PID | tail -n 1)
+  PID=$(echo $TREE | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
+
+  echo $PID
+  # If we find the working directory, run the command in that directory
+  if [ -e "/proc/$PID/cwd" ]; then
+    CWD=$(readlink /proc/$PID/cwd)
+    echo $CWD
+  fi
+fi
+if [ -n "$CWD" ]; then
+  cd $CWD && $TERM
+else
+  $TERM
+fi
---
+++ DE/i3/i3-dynamic_layout.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from i3ipc import Connection
+from sys import argv, exit
+
+if len(argv) < 2:
+    exit()
+
+i3 = Connection()
+
+focused = i3.get_tree().find_focused()
+
+if focused is not None:
+    windows = focused.workspace().leaves()
+    windows[-1].command("focus")
+    i3.command("split " + ("horizontal" if len(windows) % 2 else "vertical"))
+
+i3.command("exec " + argv[1])
