12ab9a9d8b0ec16962c572ae45782ec3821866d0
Roman Milishchuk <milromchuk@gmail.com>
2020-08-12 21:03:27 +0300

    [i3] Setted default currency to UAH in qalc and added exchange rate auto update

--- DE/i3/scripts/menu-launcher.sh
+++ DE/i3/scripts/menu-launcher.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rofi -show drun -modi drun,calc -calc-command "echo '{result}' | xclip -selection clipboard -rmlastnl" -kb-accept-custom "Control+c"
+LC_MONETARY=uk_UA.UTF-8 rofi -show drun -modi drun,calc -calc-command "echo '{result}' | xclip -selection clipboard -rmlastnl" -kb-accept-custom "Control+c"
