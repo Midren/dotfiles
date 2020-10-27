bd3edae0a7a0b6f4e6b4b928eb3e3aa8f65cc08a
Roman Milishchuk <milromchuk@gmail.com>
2020-10-27 10:56:22 +0200

    [rofi] Add shortcut for opening terminal in project folder

---
+++ DE/rofi/scripts/menu-projects.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -u
+
+readonly PROJECTS_DIR=~/.vim/temp_dirs/sessions/
+
+if [[ ! -a "${PROJECTS_DIR}" ]]; then
+    echo "empty" >> "${PROJECTS_DIR}"
+fi
+
+function get_projects()
+{
+    ls -c ${PROJECTS_DIR}
+}
+
+function main()
+{
+    project="$(get_projects 2> /dev/null | rofi -kb-custom-1 "Ctrl+o" -dmenu -p "Project:")"
+    rofi_exit=$?
+
+    if [[ $rofi_exit -eq 1 ]]; then
+		exit
+    elif [[ $rofi_exit -eq 10 ]]; then
+		cd $(echo ${project} | tr % / ) && st
+    elif [[ $rofi_exit -eq 0 ]]; then
+		export LC_ALL=en_US.UTF-8
+		cd $(echo ${project} | tr % /) && st -e vim
+	fi
+}
+
+main
