dfe41a7f95e1404bde9d163ee3b7697c9ae8e7fb
Roman Milishchuk <milromchuk@gmail.com>
2021-01-01 18:21:15 +0300

    [rofi] added correct separator

--- DE/rofi/scripts/menu-projects.sh
+++ DE/rofi/scripts/menu-projects.sh
@@ -10,22 +10,24 @@ fi
 
 function get_projects()
 {
-    ls -c ${PROJECTS_DIR}
+    projects=$(ls -c ${PROJECTS_DIR})
+    projects=${projects//%/\/}
+    echo $projects
 }
 
 function main()
 {
-    project="$(get_projects 2> /dev/null | rofi -kb-custom-1 "Ctrl+o" -dmenu -p "Project:")"
+    project="$(get_projects 2> /dev/null | rofi -kb-custom-1 "Ctrl+o" -dmenu -p "Project:" -sep " ")"
     rofi_exit=$?
 
     if [[ $rofi_exit -eq 1 ]]; then
-		exit
+        exit
     elif [[ $rofi_exit -eq 10 ]]; then
-		cd $(echo ${project} | tr % / ) && st
+        cd $(echo ${project} | tr % / ) && st
     elif [[ $rofi_exit -eq 0 ]]; then
-		export LC_ALL=en_US.UTF-8
-		cd $(echo ${project} | tr % /) && st -e vim
-	fi
+        export LC_ALL=en_US.UTF-8
+        cd $(echo ${project} | tr % /) && st -e vim
+    fi
 }
 
 main
